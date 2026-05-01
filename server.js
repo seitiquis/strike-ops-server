862da88d86327d2a564467e344e3181b7a5619de
seitiquis <64098759+seitiquis@users.noreply.github.com>
2026-05-01 15:20:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -45,3 +45,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Servidor rodando na porta", PORT);
 });
+socket.on("joinRoom", (roomId) => {
+  socket.join(roomId);
+  console.log("Player entrou na sala:", roomId);
+});
